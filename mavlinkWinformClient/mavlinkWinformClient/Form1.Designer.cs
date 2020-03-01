230a6110dcefbc5ecd0b45f8463289f70da2d048
liuguo <boots19940901@gmail.com>
2020-03-01 09:15:37 +0800

    Create the project.

---
+++ mavlinkWinformClient/mavlinkWinformClient/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace mavlinkWinformClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.serverPort = new System.Windows.Forms.TextBox();
+            this.serverIP = new System.Windows.Forms.TextBox();
+            this.connect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Server IP:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Server Port:";
+            // 
+            // serverPort
+            // 
+            this.serverPort.Location = new System.Drawing.Point(143, 75);
+            this.serverPort.Name = "serverPort";
+            this.serverPort.Size = new System.Drawing.Size(121, 25);
+            this.serverPort.TabIndex = 1;
+            // 
+            // serverIP
+            // 
+            this.serverIP.Location = new System.Drawing.Point(143, 36);
+            this.serverIP.Name = "serverIP";
+            this.serverIP.Size = new System.Drawing.Size(121, 25);
+            this.serverIP.TabIndex = 1;
+            // 
+            // connect
+            // 
+            this.connect.Location = new System.Drawing.Point(289, 67);
+            this.connect.Name = "connect";
+            this.connect.Size = new System.Drawing.Size(83, 36);
+            this.connect.TabIndex = 2;
+            this.connect.Text = "Connect";
+            this.connect.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 261);
+            this.Controls.Add(this.connect);
+            this.Controls.Add(this.serverIP);
+            this.Controls.Add(this.serverPort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox serverPort;
+        private System.Windows.Forms.TextBox serverIP;
+        private System.Windows.Forms.Button connect;
+    }
+}
+
